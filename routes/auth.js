ff17690c8912c5153e4c1e9ff5a54323829400d4
German Sarlengo <germansarlengo@gmail.com>
2026-03-31 06:46:43 -0300

    Usar bcryptjs para login/registro

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../db");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const JWT_SECRET = process.env.JWT_SECRET || "supersecretkey";
