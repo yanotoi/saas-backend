575551da2d1ada6de9fcd0e000a13a60577579ef
German Sarlengo <germansarlengo@gmail.com>
2026-04-01 03:41:26 -0300

    arreglos de CORS y rutas

--- index.js
+++ index.js
@@ -38,9 +38,11 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use((req, res, next) => {
   if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
-    return res.sendStatus(204);
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    return res.sendStatus(200); // 🔥 clave
   }
   next();
 });
